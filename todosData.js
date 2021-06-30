2bf8d68ad07c9d42cda2f23c4bc90c73e0aaeeba
PrakharMathur4 <50258233+PrakharMathur4@users.noreply.github.com>
2021-06-30 23:54:24 +0530

    Add files via upload

---
+++ todosData.js
@@ -0,0 +1,25 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Complete OS assignment",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "Work on project",
+        completed: true
+    },
+    {
+        id: 3,
+        text: "Submit the project report",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Order shoes for father",
+        completed: true
+    },
+    
+]
+
+export default todosData
